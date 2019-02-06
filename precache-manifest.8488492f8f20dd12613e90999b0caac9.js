ea973edcdfe92fa8aeff12ca11d886de2710b8ac
Alex <alex.alexeev23061991@gmail.com>
2019-02-06 18:16:36 +0300

    Updates

---
+++ precache-manifest.8488492f8f20dd12613e90999b0caac9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/Nipponsuki/weather-app.git/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "4619e5798bbdebd3a0d4",
+    "url": "/Nipponsuki/weather-app.git/static/css/main.2aa4fbcd.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Nipponsuki/weather-app.git/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "136ce592226322a60e09",
+    "url": "/Nipponsuki/weather-app.git/static/js/1.136ce592.chunk.js"
+  },
+  {
+    "revision": "5df1d4dc5475adb072cc",
+    "url": "/Nipponsuki/weather-app.git/static/js/runtime~main.5df1d4dc.js"
+  },
+  {
+    "revision": "e89bf6cf6cad8ca28985a98e0a59f5df",
+    "url": "/Nipponsuki/weather-app.git/static/media/spinner.e89bf6cf.gif"
+  },
+  {
+    "revision": "4619e5798bbdebd3a0d4",
+    "url": "/Nipponsuki/weather-app.git/static/js/main.4619e579.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Nipponsuki/weather-app.git/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Nipponsuki/weather-app.git/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Nipponsuki/weather-app.git/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Nipponsuki/weather-app.git/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "136ce592226322a60e09",
+    "url": "/Nipponsuki/weather-app.git/static/css/1.00d7b2e8.chunk.css"
+  },
+  {
+    "revision": "328e8fa2e656deddd64a1f17ec3dad6e",
+    "url": "/Nipponsuki/weather-app.git/index.html"
+  }
+];
